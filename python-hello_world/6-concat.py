906240ec7a298fdfd6bb46977a272da7bcbae83c
Trice97 <bolinp@gmail.com>
2025-05-09 12:10:58 +0200

    python-hello_world

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Holberton"  # Déclaration de la première chaîne
+str2 = "School"     # Déclaration de la deuxième chaîne
+print(f"Welcome to {str1} {str2}!")  # Affichage du message
