0f7139b7fdcb40b45f4df96e0695b20950775541
Trice97 <bolinp@gmail.com>
2025-05-11 20:14:06 +0200

    python-if_else_loops_functions

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import marshal
-import dis
 
 if __name__ == "__main__":
     try:
-        with open("hidden_4.pyc", "rb") as f:
-            # Skip the magic number and timestamp
+        with open("/tmp/hidden_4.pyc", "rb") as f:
+            # Skip the magic number and timestamp (first 8 bytes)
             f.read(8)
-            code_object = marshal.load(f)
+            code = marshal.load(f)
 
-        names = sorted([name for name in code_object.co_names if not name.startswith("__")])
+        names = sorted([name for name in code.co_names if not name.startswith("__")])
         for name in names:
             print(name)
 
