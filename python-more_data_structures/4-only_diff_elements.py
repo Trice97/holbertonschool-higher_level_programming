21bd49fbc90644b4af24e22227fa93afd7fefe48
Trice97 <bolinp@gmail.com>
2025-05-16 00:05:40 +0200

    python-more_data_structures

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    Returns a set of all elements present in only one set.
+    Args:
+        set_1: First set
+        set_2: Second set
+    Returns:
+        A set of elements that are in only one of the two sets
+    """
+    return set_1 ^ set_2
