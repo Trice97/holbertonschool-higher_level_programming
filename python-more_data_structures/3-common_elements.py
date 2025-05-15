8f968050cd27c1d2286663dd75c4f4036412b889
Trice97 <bolinp@gmail.com>
2025-05-16 00:01:51 +0200

    python-more_data_structures

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    Args:
+        set_1: First set
+        set_2: Second set
+    Returns:
+        A set containing elements common to both input sets
+    """
+    return set_1 & set_2
