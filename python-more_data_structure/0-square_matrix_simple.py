55cb3d6d81194cdd2ee76f89932eea560ef50f5e
Trice97 <bolinp@gmail.com>
2025-05-15 23:41:24 +0200

    python-more_data_structures

--- python-more_data_structure/0-square_matrix_simple.py
+++ python-more_data_structure/0-square_matrix_simple.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    """Finds the biggest integer of a list."""
-    if not my_list:
-        return None
-    biggest = my_list[0]
-    for number in my_list:
-        if number > biggest:
-            biggest = number
-    return biggest
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers of a matrix.
+    Returns a new matrix with same size as matrix where each value
+    is the square of its corresponding value in the input matrix.
+    """
+    # Create a new matrix with the same structure as the input
+    new_matrix = []
+    
+    # For each row in the matrix
+    for row in matrix:
+        # Create a new row with squared values
+        new_row = []
+        for value in row:
+            new_row.append(value ** 2)
+        # Add the new row to the new matrix
+        new_matrix.append(new_row)
+    
+    return new_matrix
