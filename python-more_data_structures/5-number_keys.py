3d3439c13b49793bfc30fbc01908da826702835d
Trice97 <bolinp@gmail.com>
2025-05-16 00:32:54 +0200

    python-more_data_structures

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    Args:
+        a_dictionary: The input dictionary
+    Returns:
+        The number of keys in the dictionary
+    """
+    return len(a_dictionary)
