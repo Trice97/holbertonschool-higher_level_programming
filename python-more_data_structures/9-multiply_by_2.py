42d451d40efb139ad067cddf7cf780e2aebe2237
Trice97 <bolinp@gmail.com>
2025-05-16 10:18:03 +0200

    python-more_data_structures

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """
+    Returns a new dictionary with all values multiplied by 2.
+    Args:
+        a_dictionary: The input dictionary with integer values
+    Returns:
+        A new dictionary with all values multiplied by 2
+    """
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
