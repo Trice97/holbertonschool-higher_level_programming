a2762afbde9afe71368af2c1d3c548d2c21696d5
Trice97 <bolinp@gmail.com>
2025-05-22 15:45:28 +0200

    python-classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new Square instance.
+        This is an empty class for now.
+        """
+        pass
