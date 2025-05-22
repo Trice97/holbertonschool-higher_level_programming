87871fcbcfa5efab737bd927bb3b44a8be735f21
Trice97 <bolinp@gmail.com>
2025-05-22 16:28:14 +0200

    python-classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """création of the class Square"""
+
+    def __init__(self, size):
+        """creation of a new square instance"""
+
+        self.__size = size
