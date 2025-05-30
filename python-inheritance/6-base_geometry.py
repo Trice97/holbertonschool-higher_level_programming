e547393fbcc8dd08fbc4edd03227d26bae132469
Trice97 <bolinp@gmail.com>
2025-05-30 19:23:26 +0200

    python_inheritance

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module docstring."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Area method that raises an exception."""
+        raise Exception("area() is not implemented")
