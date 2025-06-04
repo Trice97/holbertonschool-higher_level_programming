0ebd7ba5c7e011b778738d6b7e707a1a3967fa0c
Trice97 <bolinp@gmail.com>
2025-06-04 12:10:20 +0200

    python-input_output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
 """module to write a function that read a utf-8 text and prints its stdout"""
 
 
 def read_file(filename=""):
     """ Function allowing the reading of the file"""
-
+i
     with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
