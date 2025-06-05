5802023510b936b967f424de5ac99617f596060b
Trice97 <bolinp@gmail.com>
2025-06-05 12:44:04 +0200

    python-input_output

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module regarding JSON deserialization"""
+
+import json
+
+def from_json_string(my_str):
+    """Function that converts JSON string to Python object"""
+    return json.loads(my_str)
