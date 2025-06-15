d6c98beb1a978385d266bd796d944552d19d11cd
Trice97 <bolinp@gmail.com>
2025-06-15 21:53:26 +0200

    python-input_output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""" Module about adding argument to a Python list"""
+"""
+Module to add some arguments to a Python list and then save them to a file
+"""
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv[1:]
 filename = "add_item.json"
+args = sys.argv[1:]
 
 try:
-    new_list = load_from_json_file(filename)
-    except File_doesnt_exist:
-        new_list = []
-    new_list.extend(agrv[1:])
-    save_to_json_file(new_list, filename)
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(args)
+save_to_json_file(my_list, filename)
