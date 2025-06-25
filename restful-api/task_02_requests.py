f031f199ab6460f312b6de4089b58ebb8bf0fcb0
Trice97 <bolinp@gmail.com>
2025-06-25 18:05:34 +0200

    restful-api

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,37 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        structured_posts = []
+
+        for post in posts:
+            structured_post = {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+            structured_posts.append(structured_post)
+
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_posts)
