a5d9cf234b142b39bfb108558a87b9b630f3afdf
Trice97 <bolinp@gmail.com>
2025-07-31 12:22:45 +0200

    mise à jour js late tasks

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 // Récupère l'argument de la ligne de commande et le convertit en entier
 const x = parseInt(process.argv[2]);
+
 // Vérifie si la conversion a échoué
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
@@ -10,3 +11,4 @@ if (isNaN(x)) {
     console.log('C is fun');
   }
 }
+
