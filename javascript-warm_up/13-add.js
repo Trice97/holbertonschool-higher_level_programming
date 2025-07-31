17ff6d5400fe02970840c3ea709246b81300f3d2
Trice97 <bolinp@gmail.com>
2025-07-31 12:08:33 +0200

    mise à jour js late tasks

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,9 @@
-exports.add = function (a, b) {
+#!/usr/bin/node
+
+// Définition de la fonction add
+function add (a, b) {
   return a + b;
-};
+}
+
+// Exportation pour l'utiliser dans d'autres fichiers
+module.exports = { add };
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
-if (isNaN(n)) {
+
+// Récupère l'argument de la ligne de commande et le convertit en entier
+const x = parseInt(process.argv[2]);
+
+// Vérifie si la conversion a échoué
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < n; i++) {
+  // Boucle for pour afficher "C is fun" x fois
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
