a6df980782c39243c3ce90238e262e13520f4155
Trice97 <bolinp@gmail.com>
2025-07-31 12:28:05 +0200

    mise à jour js late tasks

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (isNaN(n) || n === 0) {
-    return 1;
-  }
+
+// Fonction récursive pour calculer la factorielle
+function factorial (n) {
+  if (isNaN(n) || n < 0) return 1;
+  if (n === 0) return 1;
   return n * factorial(n - 1);
 }
+
+// Récupère l'argument et le convertit en entier
 const n = parseInt(process.argv[2]);
+
+// Affiche le résultat
 console.log(factorial(n));
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-function add(a, b) {
+
+// Définition de la fonction d'addition
+function add (a, b) {
   return a + b;
 }
+
+// Récupération et conversion des arguments en entiers
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
+
+// Affichage du résultat
 console.log(add(a, b));
