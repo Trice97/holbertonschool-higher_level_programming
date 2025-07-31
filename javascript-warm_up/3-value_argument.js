f42ca7ba347b6c91565a8f1c05106493f9513e32
Trice97 <bolinp@gmail.com>
2025-07-31 12:25:25 +0200

    mise à jour js late tasks

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-// Récupère le premier argument passé (après le nom du script)
 const arg = process.argv[2];
 
-// Vérifie si l'argument existe
 if (arg === undefined) {
   console.log('No argument');
 } else {
